b8d79b18cb800ba571cc2c935ec929b628c78f00
mehdilagdimi <lagdimi.mehdi@gmail.com>
2022-05-09 14:57:19 +0100

    fixed record ui and added selected bg color change

--- src/Components/Record.jsx
+++ src/Components/Record.jsx
@@ -1,44 +1,49 @@
-import { useState } from 'react'
-import AddForm from './AddForm';
-import Button from "./Button"
-import Report from './Report';
+import { useState, useEffect } from "react";
+import AddForm from "./AddForm";
+import Button from "./Button";
+import Report from "./Report";
 
-
-
-const Record = ({onClickEdit, btnsLabel}) => {
-  const [value, setValue] = useState()
-  const [showReport, setShowReport] =  useState(false); 
-  const [showUpdateForm, setShowUpdateForm] = useState(false)
+const Record = ({ onClickEdit, btnsLabel }) => {
+  const [value, setValue] = useState();
+  const [showReport, setShowReport] = useState(false);
+  const [showUpdateForm, setShowUpdateForm] = useState(false);
+  const [selected, setSelected] = useState(false);
   const getReport = (report) => {
-    console.log(report)
-  }
-  console.log(btnsLabel[0])
-//   const onClick = (e) => {
-//     onClickEdit(e)
-// console.log(e)
-//   }
+    console.log(report);
+  };
+  console.log(btnsLabel[0]);
+  //   const onClick = (e) => {
+  //     onClickEdit(e)
+  // console.log(e)
+  //   }
+  useEffect(() => {}, [selected]);
 
   return (
     <>
-       <tr className="bg-navGray text-center">
-          <td className="py-2 px-6 text-left rounded-l-lg"> Patient</td>
-          <td className="py-2 px-6"> Order Order Order</td>
-          <td className="py-2 px-6">Patient</td>
-          <td className="py-2 px-6">Pending</td>
-          <td className="py-2 px-6">Radiologist</td>
-          <td className="h-full w-40 py-2 px-1 text-right">
-            <div className="flex justify-end  p-0 m-0">
+      <tr className={`${selected ? "bg-gray-300" : "bg-navGray"} text-center`}>
+        <td className="py-2 px-6 text-left rounded-l-lg"> Patient</td>
+        <td className="py-2 px-6"> Order Order Order</td>
+        <td className="py-2 px-6">Patient</td>
+        <td className="py-2 px-6">Pending</td>
+        <td className="py-2 px-6">Radiologist</td>
+        <td className="h-full w-40 py-2 px-1 text-right">
+          <div className="flex justify-end  p-0 m-0">
             <div className="mx-2 inline-block">
-              <Button label={btnsLabel[0]} onClick={() => setShowReport(!showReport)} />
+              <Button
+                label={btnsLabel[0]}
+                onClick={() => (
+                  setShowReport(!showReport), setSelected(!selected)
+                )}
+              />
             </div>
             <div className="mx-2 inline-block">
-              <Button label={btnsLabel[1]} onClick={onClickEdit}/>
+              <Button label={btnsLabel[1]} onClick={onClickEdit} />
             </div>
-            </div>
-          </td>
-        </tr>
-        {showReport && <Report user={"physician"} getReport={getReport}/> }
-    </> 
+          </div>
+        </td>
+      </tr>
+      {showReport && <Report user={"physician"} getReport={getReport} />}
+    </>
   );
 };
 
--- src/Components/Report.jsx
+++ src/Components/Report.jsx
@@ -25,11 +25,11 @@ const Report = ({ user, getReport }) => {
     // <div className="w-full flex justify-between items-center">
 
     <>
-      <tr className="text-center h-96">
+      <tr className="text-center h-full">
         <td colSpan="3" className="pb-5 h-96 2xl:pr-16">
           {/* <div className="h-full bg-navGray"></div> */}
-          <h3 className="bg-white my-1">Radiologist Report</h3>
-          <div className="flex flex-col justify-between min-h-full h-full bg-navGray border border-gray-300 rounded-md p-3">
+          <h3 className="my-1">Radiologist Report</h3>
+          <div className="flex flex-col justify-between min-h-full h-5/6 bg-navGray border border-gray-300 rounded-md p-3">
           <form className="h-full w-full" onSubmit={(e) => e.preventDefault()}>
               <textarea
                 disabled={(user == "radiologist" ? false : true)}
@@ -61,7 +61,7 @@ const Report = ({ user, getReport }) => {
           </div>
         </td>
         <td colSpan="3" className="pb-5 h-96 pl-10">
-          <h3 className="bg-white my-1 ml-20">Physician Report</h3>
+          <h3 className="my-1 w-full">Physician Report</h3>
           <div className="flex flex-col justify-between min-h-full h-full bg-navGray border border-gray-300 rounded-md p-3">
             <form className="h-full w-full" onSubmit={(e) => e.preventDefault()}>
               <textarea
